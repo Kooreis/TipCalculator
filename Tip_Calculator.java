136f67d7eac71be35904b2b893392d0d8df0d5c7
Kooreis <kooreis.official@gmail.com>
2022-07-12 18:00:00 -0500

    Initial setup of the TipCalculator class and main method. A Scanner object is created to take user input.

--- Tip_Calculator.java
+++ Tip_Calculator.java
@@ -1,17 +1,7 @@
-```java
 import java.util.Scanner;
 
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the bill amount: ");
-        double bill = scanner.nextDouble();
-        System.out.println("Enter the tip percentage: ");
-        double tipPercentage = scanner.nextDouble();
-        double tip = bill * (tipPercentage / 100);
-        double total = bill + tip;
-        System.out.println("The tip amount is: " + tip);
-        System.out.println("The total bill amount is: " + total);
     }
 }
-```

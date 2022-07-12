fd07924bf55d87b5f3fe002bbc7ca06aee3fb24f
Kooreis <kooreis.official@gmail.com>
2022-07-12 18:00:00 -0500

    This update adds the function definition for tip_calculator and includes the user input for the total bill amount.

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -1,13 +1,2 @@
 def tip_calculator():
     bill = float(input("Enter the total bill amount: "))
-    tip_percentage = float(input("Enter the tip percentage: "))
-    num_people = int(input("Enter the number of people: "))
-
-    tip_amount = bill * (tip_percentage / 100)
-    total_bill = bill + tip_amount
-    bill_per_person = total_bill / num_people
-
-    print("Total bill: ", total_bill)
-    print("Bill per person: ", bill_per_person)
-
-tip_calculator()

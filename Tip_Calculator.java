ae8e36f99566c72d459f9d13f0b62c2f0326b470
Kooreis <kooreis.official@gmail.com>
2022-07-13 19:00:00 -0500

    Added a prompt for the user to enter the bill amount and stored the input in a variable.

--- Tip_Calculator.java
+++ Tip_Calculator.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the bill amount: ");
+        double bill = scanner.nextDouble();
     }
 }

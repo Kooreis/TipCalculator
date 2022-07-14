c97e97df4b46abe367341cd0b53a5dc8f4a9aa03
Kooreis <kooreis.official@gmail.com>
2022-07-13 19:00:00 -0500

    This update adds the user input for the tip percentage and the number of people.

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -1,2 +1,2 @@
-def tip_calculator():
-    bill = float(input("Enter the total bill amount: "))
+tip_percentage = float(input("Enter the tip percentage: "))
+    num_people = int(input("Enter the number of people: "))

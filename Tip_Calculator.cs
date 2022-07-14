7112cf8db5b9486019bf875589116ac2f13318e2
Kooreis <kooreis.official@gmail.com>
2022-07-13 19:00:00 -0500

    In this update, the user is prompted to enter the tip percentage. The input is then converted to a double data type and stored in the 'tipPercentage' variable.

--- Tip_Calculator.cs
+++ Tip_Calculator.cs
@@ -6,5 +6,8 @@ class Program
     {
         Console.Write("Enter the total bill amount: ");
         double bill = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the tip percentage: ");
+        double tipPercentage = Convert.ToDouble(Console.ReadLine());
     }
 }
